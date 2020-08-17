565558e3b90db599ac97df62f93fb454960cd499
= <=>
2020-08-17 12:13:46 -0500

    ahora se pueden eliminar los links

--- src/routes/links.js
+++ src/routes/links.js
@@ -23,7 +23,13 @@ router.get('/', async (req,res) => {
     const links = await pool.query('SELECT * FROM links');
     console.log(links);
     res.render('links/list', { links });
-})
+});
+
+router.get('/delete/:id', async (req,res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE id = ?', [id]);
+    res.redirect('/links');
+});
 
 module.exports = router;
 
