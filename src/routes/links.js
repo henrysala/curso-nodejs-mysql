c8f8bc6eec91e1e934ee9a72b002d9419b303a53
= <=>
2020-08-17 15:12:20 -0500

    Ahora se pueden editar los enlaces

--- src/routes/links.js
+++ src/routes/links.js
@@ -31,5 +31,24 @@ router.get('/delete/:id', async (req,res) => {
     res.redirect('/links');
 });
 
+router.get('/edit/:id', async (req,res) => {
+    const { id } = req.params;
+    const link = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    res.render('links/edit', {link: link[0]});
+});
+
+router.post('/edit/:id', async (req,res) => {
+    const { id } = req.params;
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    console.log(newLink)
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    res.redirect('/links');
+});
+
 module.exports = router;
 
